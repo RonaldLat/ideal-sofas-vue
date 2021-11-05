1e829923213c3c28a410473b53c3d781e779ead1
Ronald <ronnlats@gmail.com>
2021-11-05 11:47:12 +0300

    Whatsapp number update again

--- src/components/Whatsapp.vue
+++ src/components/Whatsapp.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <a
-    href="https://api.whatsapp.com/send?phone=2547180622381&text="
+    href="https://api.whatsapp.com/send?phone=254718062381&text="
     class="fixed z-50 w-12 h-12 bottom-2 right-2"
   >
     <img src="/images/whatsapp.svg" alt="" class="w-full h-full" />
