bb17696bd2f30f6e19896d887a803168fb500111
Ronald Otieno <ronnlats@gmail.com>
2021-11-05 04:21:18 +0300

    Update index.js
    Try fix scroll bug

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,13 @@ let routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+scrollBehavior (to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition
+  } else {
+    return { x: 0, y: 0 }
+  }
+}
 });
 
 export default router;
